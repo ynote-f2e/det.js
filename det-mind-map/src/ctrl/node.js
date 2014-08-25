4ae424288cf34b966afe1fd51709bc007063d06c
shihongzhi <shi65881583@gmail.com>
2014-08-26 00:09:49 +0800

    add sample style

--- det-mind-map/src/ctrl/node.js
+++ det-mind-map/src/ctrl/node.js
@@ -38,12 +38,40 @@ var MindNodeCtrl = (function (GraphCtrl, DragDropFeature) {
                 model = this.getModel(),
                 parentCtrl = this.getParent(),
                 parentModel = parentCtrl.getModel(),
-                parentFigure = parentCtrl.getFigure();
-            this.rect = paper.rect(model.get('x'),
-                model.get('y'),
-                model.get('width'),
-                model.get('height'));
-            this.text = paper.text(model.get('x'), model.get('y'), model.get('text'));
+                parentFigure = parentCtrl.getFigure(),
+                x = model.get('x'),
+                y = model.get('y'),
+                width = model.get('width'),
+                height = model.get('height'),
+                text = model.get('text'),
+                strokeWidth = 3;
+
+            this.rect = paper.rect(x, y, width, height);
+            if (_.isFunction(parentModel.get)) {
+                this.line = paper.line(x, 
+                            y,
+                            parentModel.get('x'),
+                            parentModel.get('y') + parentModel.get('height') / 2);
+                this.line.attr({
+                    stroke: '#00ADEF'
+                })
+                this.setLinePosition(x, y, width, height,
+                                parentModel.get('x'),
+                                parentModel.get('y'),
+                                parentModel.get('width'),
+                                parentModel.get('height'));
+            }
+
+            this.text = paper.text(x, y + height - strokeWidth - 2, text);
+
+            //model.set('width', this.text.node.clientWidth);
+            this.rect.attr({
+                'fill-opacity': 0,
+                width: (this.text.node.clientWidth),
+                stroke: "#bada55",
+                strokeWidth: strokeWidth
+            })
+
             return paper.group(this.rect, this.text);
         },
 
@@ -52,13 +80,31 @@ var MindNodeCtrl = (function (GraphCtrl, DragDropFeature) {
          * */
         refreshFigure : function () {
             var figure = this.getFigure(),
-                model = this.getModel();
-            this.rect.attr('x', model.get('x'))
-                .attr('y', model.get('y'))
-                .attr('width', model.get('width'))
-                .attr('height', model.get('height'));
-            this.text.attr('x', model.get('x'))
-                .attr('y', model.get('y'));
+                model = this.getModel(),
+                parentCtrl = this.getParent(),
+                parentModel = parentCtrl.getModel(),
+                x = model.get('x'),
+                y = model.get('y'),
+                width = model.get('width'),
+                height = model.get('height'),
+                text = model.get('text'),
+                strokeWidth = 3;
+            this.text.attr({
+                x: x,
+                y: y  + height - strokeWidth - 2
+            });
+            this.rect.attr({
+                'fill-opacity': 0,
+                x: x,
+                y: y
+            });
+            if (_.isFunction(parentModel.get)) {
+                this.setLinePosition(x, y, width, height,
+                                parentModel.get('x'),
+                                parentModel.get('y'),
+                                parentModel.get('width'),
+                                parentModel.get('height'));
+            }
         },
 
         /**
@@ -67,8 +113,84 @@ var MindNodeCtrl = (function (GraphCtrl, DragDropFeature) {
         getModelChildren : function () {
             var model = this.getModel();
             return model.nodes;
-        }
+        },
 
+        setLinePosition: function(x, y, w, h, px, py, pw, ph) {
+            var center = {
+                    x: x + w / 2,
+                    y: y + h / 2
+                },
+                pcenter = {
+                    x: px + pw / 2,
+                    y: py + ph / 2
+                };
+
+            if (center.x >= pcenter.x && center.y >= pcenter.y) {
+                if (center.x - pcenter.x >= center.y - pcenter.y) {
+                    this.line.attr({
+                        x1: px + pw,
+                        y1: py + ph / 2,
+                        x2: x,
+                        y2: y + h / 2
+                    });
+                } else {
+                    this.line.attr({
+                        x1: px + pw / 2,
+                        y1: py + ph,
+                        x2: x,
+                        y2: y + h / 2
+                    });
+                }
+            } else if (center.x >= pcenter.x && center.y < pcenter.y) {
+                if (center.x - pcenter.x >= pcenter.y - center.y) {
+                    this.line.attr({
+                        x1: px + pw,
+                        y1: py + ph / 2,
+                        x2: x,
+                        y2: y + h / 2
+                    });
+                } else {
+                    this.line.attr({
+                        x1: px + pw / 2,
+                        y1: py,
+                        x2: x,
+                        y2: y + h / 2
+                    });
+                }
+            } else if (center.x < pcenter.x && center.y >= pcenter.y) {
+                if (pcenter.x - center.x >= center.y - pcenter.y) {
+                    this.line.attr({
+                        x1: px,
+                        y1: py + ph / 2,
+                        x2: x + w,
+                        y2: y + h / 2
+                    });
+                } else {
+                    this.line.attr({
+                        x1: px + pw / 2,
+                        y1: py + ph,
+                        x2: x + w,
+                        y2: y + h / 2
+                    });
+                }
+            } else {
+                if (pcenter.x - center.x >= pcenter.y - center.y) {
+                    this.line.attr({
+                        x1: px,
+                        y1: py + ph / 2,
+                        x2: x + w,
+                        y2: y + h / 2
+                    });
+                } else {
+                    this.line.attr({
+                        x1: px + pw / 2,
+                        y1: py,
+                        x2: x + w,
+                        y2: y + h / 2
+                    });
+                }
+            }
+        }
     });
 
 }(det.GraphCtrl, det.DragDropFeature));
