421575f185ca53d2153df36ce87db77b544907ef
DreamFox <dreamfox@vip.qq.com>
2014-08-29 18:00:06 +0800

    MOD: test

---
+++ det-mind-map/src/cmd/setBackgroundColor.js
@@ -0,0 +1,43 @@
+detMindMap.SetBackGroundColorCommand = (function (det) {
+
+    'use strict';
+
+    return det.derive(function (model, color) {
+        this.model = model;
+        this.color = color;
+        this.oldColor = model.get('lineAttr').stroke;
+    }, {
+
+        setLineColor : function (model, color) {
+            model.setLineAttr('stroke', color);
+            model.setLineAttr('fill', color);
+            model.setRectAttr('stroke', color);
+        },
+
+        paintChildren : function (model, color) {
+            if (!model) {
+                return;
+            }
+            var node, nodes = [].concat(model.nodes);
+            this.setLineColor(model, color);
+            while (nodes.length) {
+                node = nodes.shift();
+                this.paintChildren(node, color);
+            }
+        },
+
+        execute : function () {
+            this.paintChildren(this.model, this.color);
+        },
+
+        undo : function () {
+            this.paintChildren(this.model, this.oldColor);
+        },
+
+        redo : function () {
+            this.paintChildren(this.model, this.color);
+        }
+
+    });
+
+}(det));
